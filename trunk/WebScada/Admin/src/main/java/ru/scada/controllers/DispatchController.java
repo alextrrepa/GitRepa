5be6e05fe27751bb7180f8a76ac32194c6afa1b6
alextrrepa <alextrrepa@e8dfc67e-2a6e-4f3e-b76f-b44e09974a2d>
2015-08-31 08:10:29 +0000

    В Admin части создал dispatch_delegation.

---
+++ trunk/WebScada/Admin/src/main/java/ru/scada/controllers/DispatchController.java
@@ -0,0 +1,36 @@
+package ru.scada.controllers;
+
+import ru.scada.controllers.dispatch_delegation.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DispatchController extends HttpServlet {
+    private Map<String, Command> commandMap = new HashMap<>();
+
+    public DispatchController() {
+        Dispatch dispatch = new Dispatch();
+        commandMap.put("modbusPage", new ModbusDispatch(dispatch));
+        commandMap.put("adminPart", new AdminDispatch(dispatch));
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        performTask(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        performTask(request, response);
+    }
+
+    private void performTask(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        Command command = commandMap.get(action);
+        DoDispatch doDispatch = new DoDispatch(command);
+        doDispatch.makeDispatch(request, response);
+    }
+}
