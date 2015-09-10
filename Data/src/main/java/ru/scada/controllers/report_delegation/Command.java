a3afbecc003b68047261a3ff2d6d9f07b1601616
alextrrepa <alextrrepa@e8dfc67e-2a6e-4f3e-b76f-b44e09974a2d>
2015-09-10 10:35:34 +0000

    Для плагина jtable добавил бэк энд. Что-то не работает пока(

---
+++ Data/src/main/java/ru/scada/controllers/report_delegation/Command.java
@@ -0,0 +1,9 @@
+package ru.scada.controllers.report_delegation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface Command {
+    void execute(HttpServletRequest request, HttpServletResponse response) throws IOException;
+}
--- Data/src/test/java/DaoTest.java
+++ Data/src/test/java/DaoTest.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 public class DaoTest {
     @Test
     public void hourTest() {
-
+        /*GenericDao<HourEntity, Long> dao = new ItemDao<>();
+        List<HourEntity> hours = dao.showHoursData();
+        for (HourEntity h : hours) {
+            System.out.println(h.getDtime() + " " + h.getValue());
+        }*/
     }
 }
